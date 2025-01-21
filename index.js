566340771793887993157d2abccb73e3a4f0f9b0
an2niy85 <an2niy85@gmail.com>
2025-01-21 22:46:35 +0400

    Исправил ошибку в формуле js файла, перенес подключение файла js в конец

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const positionLong = 10;
 const addressLat = 20;
 const addressLong = 20;
 
-const result = ((addressLat - positionLat)**2 + (positionLat - positionLong)**2)**(1/2);
+const result = ((addressLat - positionLat)**2 + (addressLong - positionLong)**2)**(1/2);
 console.log(`Расстояние от текущей точки до точки назначения равно ${result.toFixed(2)}`);
