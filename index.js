24f35297530f7b343dc19817f288c0567b17d39d
an2niy85 <an2niy85@gmail.com>
2025-01-21 22:41:04 +0400

    Добавил решение ДЗ2, файл js и подключил его к html

---
+++ index.js
@@ -0,0 +1,7 @@
+const positionLat = 10;
+const positionLong = 10;
+const addressLat = 20;
+const addressLong = 20;
+
+const result = ((addressLat - positionLat)**2 + (positionLat - positionLong)**2)**(1/2);
+console.log(`Расстояние от текущей точки до точки назначения равно ${result.toFixed(2)}`);
